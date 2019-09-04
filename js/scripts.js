26c26bcfc312be0678f85a8c6de106067a23386b
John-5014 <kaiserjohn52@gmail.com>
2019-09-04 11:24:18 +0300

    add carousel and about section with styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,20 @@ $(document).ready(function(){
   $("#hover1").hover(function(){
     $("#show1").fadeToggle()
   });
+  var myIndex = 0;
+carousel();
+
+function carousel(){
+  var i;
+  var x = document.getElementsByClassName("item");
+  for (i = 0; i < x.length; i++){
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length){
+    myIndex = 1
+  }
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel,4000);
+}
 });
